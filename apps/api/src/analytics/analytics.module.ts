75f7b9ffce32feafda98a3e611cfe8912bae1798
robi-sumitro <robisumitro@gmail.com>
2026-08-05 03:13:01 +0700

    fix(api): import SecurityModule in AnalyticsModule for JwtAuthGuard

--- apps/api/src/analytics/analytics.module.ts
+++ apps/api/src/analytics/analytics.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AnalyticsService } from './analytics.service';
 import { AnalyticsController } from './analytics.controller';
 import { PlatformsModule } from '../platforms/platforms.module';
+import { SecurityModule } from '../common/security.module';
 
 @Module({
-  imports: [PlatformsModule],
+  imports: [PlatformsModule, SecurityModule],
   controllers: [AnalyticsController],
   providers: [AnalyticsService],
   exports: [AnalyticsService],
